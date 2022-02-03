f422246235e12437c444bf189b2f51a6b0d0ad39
bossxerr <70553957+Bossxer@users.noreply.github.com>
2022-02-02 23:28:25 -0600

    Update Function.lua

--- Function.lua
+++ Function.lua
@@ -1,2 +1,17 @@
-_G.print = "ong no cap no fuckery"
+_G.CoinCount = 0
 
+
+function VipCheck()
+    if game.Players.LocalPlayer.PlayerGui.MainGui.InGameSidebar.CoinCountMax.Text == "/ 150" then
+            _G.CoinCount = 150
+        else do
+            _G.CoinCount = 50
+        end
+    end
+end
+
+function RevolverDroppedCheck()
+   if game.Workspace:FindFirstChild("RevolverPickUp") ~= nil then
+        game.Workspace.RevolverPickUp.Orb.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+    end
+end
